da44b9e6170388d2401419737a3c195682f01da3
Cristina Luengo <cristinaluengoagullo@gmail.com>
2016-03-24 13:04:19 +0100

    New files from both labs

---
+++ Graph coloring/run_script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TIME_LIMIT=15 # In secs.
+BENCH_DIR=instances
+EXE=p
+
+######## DO NOT CHANGE BELOW THIS LINE ########
+
+ulimit -t $TIME_LIMIT
+
+n_solved=0
+n_all_pb=0
+total_time=0
+for ifile in $BENCH_DIR/*.pb; do
+    ofile=$ifile.out
+    /usr/bin/time -f "%e" $EXE $ifile &> tmp.txt
+    if [ $? != 0 ]; then
+	echo "Timed out: $ifile"
+    else
+	echo "OK       : $ifile"
+	n_solved=$(( n_solved+1 ))
+	time=$(tail -n 1 tmp.txt)
+	total_time=$(echo "$total_time + $time" | bc)
+    fi
+    n_all_pb=$(( n_all_pb+1 ))
+done
+echo "Time solved problems: $total_time"
+echo "Num. solved problems: $n_solved"
+echo "Num.  all   problems: $n_all_pb"
+perc=$(echo "scale=2; (100*$n_solved)/$n_all_pb" | bc )
+echo "Percentage: $perc %"
+rm --force tmp.txt
